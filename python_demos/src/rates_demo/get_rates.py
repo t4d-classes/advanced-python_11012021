48dfb061cab18313d6ecd8ad317d4b5a9618b2ce
Eric Greene <eric@t4d.io>
2021-11-02 20:01:48 -0400

    add solution to exercise 3

--- python_demos/src/rates_demo/get_rates.py
+++ python_demos/src/rates_demo/get_rates.py
@@ -1,7 +1,8 @@
 """ get rates """
 
-from concurrent.futures import ThreadPoolExecutor
+# from concurrent.futures import ThreadPoolExecutor
 from datetime import date, timedelta
+import threading
 
 import requests
 
@@ -31,7 +32,7 @@ def get_rates() -> None:
     print("\n".join(rate_responses))
 
 
-def get_rate_task(business_day: date):
+def get_rate_task(business_day: date, responses: list[str]) -> None:
     """ get rate task """
 
     rate_url = "".join([
@@ -41,24 +42,58 @@ def get_rate_task(business_day: date):
     ])
 
     response = requests.get(rate_url)
-    return response.text
+    responses.append(response.text)
 
 
-def get_rates_threadpool() -> None:
-    """ get rates threadpool """
+def get_rates_threaded() -> None:
+    """ get rates """
+    print("get rates")
 
     start_date = date(2021, 1, 1)
     end_date = start_date + timedelta(days=20)
 
     rate_responses: list[str] = []
+    threads: list[threading.Thread] = []
 
-    with ThreadPoolExecutor() as executor:
-        rate_responses = list(executor.map(
-            get_rate_task,
-            [ business_day for business_day
-              in business_days(start_date, end_date) ]))
+    for business_day in business_days(start_date, end_date):
+        a_thread = threading.Thread(
+            target=get_rate_task, args=(business_day, rate_responses))
+        a_thread.start()
+        threads.append(a_thread)
 
-    print("".join(rate_responses))
+    for a_thread in threads:
+        a_thread.join()
+
+    print("\n".join(rate_responses))
+
+# def get_rate_task(business_day: date):
+#     """ get rate task """
+
+#     rate_url = "".join([
+#         "http://127.0.0.1:5000/api/",
+#         str(business_day),
+#         "?base=USD&symbols=EUR"
+#     ])
+
+#     response = requests.get(rate_url)
+#     return response.text
+
+
+# def get_rates_threadpool() -> None:
+#     """ get rates threadpool """
+
+#     start_date = date(2021, 1, 1)
+#     end_date = start_date + timedelta(days=20)
+
+#     rate_responses: list[str] = []
+
+#     with ThreadPoolExecutor() as executor:
+#         rate_responses = list(executor.map(
+#             get_rate_task,
+#             [ business_day for business_day
+#               in business_days(start_date, end_date) ]))
+
+#     print("".join(rate_responses))
 
 
 
